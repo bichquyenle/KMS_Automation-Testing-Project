8f218549f80bc8d5727aaa94c6201ae4f2f4b773
Khánh Nhi <nhi.tuong25@student.passerellesnumeriques.org>
2025-01-09 11:05:00 +0700

    write script of feature create blog

---
+++ Object Repository/BLOG_PAGE/txt_YourName.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_YourName</name>
+   <tag></tag>
+   <elementGuidId>8cd7eb28-3333-4957-aaa4-53f5f675509d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='name']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
